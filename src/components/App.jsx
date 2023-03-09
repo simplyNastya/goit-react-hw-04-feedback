f1ddee1cc70c4432976f2c887d8080d41ab361a4
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-03-09 16:09:19 +0100

    Refactored code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { Component } from 'react';
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
